d106455c1f9e34b6fc94e11d0c5dbc7ccac14d7b
PRINCE RAJ <rajprince031@gmail.com>
2024-06-06 23:42:55 +0530

    Color correction

--- lib/Decoration/decoration.dart
+++ lib/Decoration/decoration.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 
 final todo_box = BoxDecoration(
   gradient:
@@ -12,13 +14,11 @@ final todo_box = BoxDecoration(
 
 );
 
-final box_deco = BoxDecoration(
-  gradient:SweepGradient(
-    colors: [Color(0xffe5deb6), Color(0xffecb8b8), Color(0xffc94848), Color(
-        0xff544d4d)],
-    stops: [0, 0.34, 0.67, 1],
-    center: Alignment.topRight,
-  ),
+final box_deco =  BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.green, Colors.blue]),
 
    borderRadius:BorderRadius.circular(20),
 );
